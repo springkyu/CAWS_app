1c51742e1475bd29903690360606ab7bab4edbd6
springkyu <seika.k.r0610@gmail.com>
2021-09-09 07:33:25 +0000

    “[UPDATE]devise/管理者のルーティング追加”

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, contollers: {
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
+
   root to: "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
