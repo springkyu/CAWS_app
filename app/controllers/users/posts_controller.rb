5dfcf4743bc4b8197464491e7b681c802a64b2af
springkyu <seika.k.r0610@gmail.com>
2021-09-09 16:58:18 +0000

    “[UPDATE]postsコントローラ編集中”

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,2 +1,37 @@
 class Users::PostsController < ApplicationController
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save!
+      flash[:notice]="You have creatad book successfully."
+      redirect_to post_path(@post)
+    else
+      redirect_back(fallback_location: root_path)
+      # @user = current_user
+      # @posts = Post.all
+      # render :index
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:shop_name, :image_id, :body, :location)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
