bf67e9d41ac26ae6dfa59482fe29bcc815b24c66
springkyu <seika.k.r0610@gmail.com>
2021-09-09 07:46:30 +0000

    “[ADD]Likeモデル作成、カラム追加”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_074339) do
+ActiveRecord::Schema.define(version: 2021_09_09_074451) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_09_09_074339) do
 
   create_table "areas", force: :cascade do |t|
     t.string "area", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
