7e2f6b66e3ef195a9af899f09fcbac25e4fe0a41
springkyu <seika.k.r0610@gmail.com>
2021-09-09 16:58:25 +0000

    “[UPDATE]postsコントローラ編集中”

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -15,15 +15,35 @@ class Users::PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
+    @post = Post.new
+    @user = current_user
   end
 
   def show
+    @newpost = Post.new
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
 
   def edit
+    @post = Post.find(params[:id])
+    if @post.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.user_id = current_user.id
+    if @post.update(post_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_back post_path(@post.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
