b86d7b95487e7cce587b63306a9264689e71902e
springkyu <seika.k.r0610@gmail.com>
2021-09-10 10:06:45 +0000

    “has_one_attachedメソッド記述”

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,6 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :image
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_one_attached :profile_image
+
 end
