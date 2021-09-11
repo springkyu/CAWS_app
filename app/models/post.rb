33ef0746de2dcdc4623626e9fb87daafb95d8114
springkyu <seika.k.r0610@gmail.com>
2021-09-11 07:22:38 +0000

    “ActiveStorageバリデーション実装”

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,16 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
+  # ActiveStorageバリデーション
+  validate :image_type
+
+  private
+
+  def image_type
+    if !image.blob.content_type.in?(%('image/jpg image/jpeg image/png'))
+      image.purge
+      errors.add(:image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+    end
+  end
+
 end
