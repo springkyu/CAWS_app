f5707a2c9e115338c3d3302ba4e736de260f51d4
springkyu <seika.k.r0610@gmail.com>
2021-09-11 17:02:58 +0000

    [edit]commentsコントローラ編集

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,6 +1,11 @@
 class Users::CommentsController < ApplicationController
 
   def create
+    post = Post.find(params[:post_id])
+    comment = current_user.comment.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    render :index
   end
 
   def edit
@@ -10,6 +15,15 @@ class Users::CommentsController < ApplicationController
   end
 
   def destroy
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    render :index
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 
 end
