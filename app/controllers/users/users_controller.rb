8908999d1acd3d2cd2317539ab71bcc22c8e61e0
springkyu <seika.k.r0610@gmail.com>
2021-09-13 07:07:09 +0000

    [edit]userコントローラ編集中

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,19 +1,38 @@
 class Users::UsersController < ApplicationController
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to users_path
+    else
+      flash[:alert] = "Could not update."
+      render :edit
+    end
   end
 
   def unsubscribe
+    @user = current_user
   end
 
   def withdraw
+    @user = current_user
+    @user.update(is_deleted: true)
+      reset_session
+      redirect_to root_path
   end
 
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :email, :is_deleted)
+  end
 end
