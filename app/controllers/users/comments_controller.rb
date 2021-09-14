619d5d6d7187de912c7ca8ef5a040a35cd2a4138
springkyu <seika.k.r0610@gmail.com>
2021-09-14 17:49:43 +0000

    [edit]user/コメント非同期実装中、リロードしないと反映されない

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -5,24 +5,24 @@ class Users::CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.post_id = post.id
     if comment.save!
-      redirect_back(fallback_location: root_path)
+      # redirect_back(fallback_location: root_path)
       # 非同期
-      # render :index
+      render :index
     end
   end
 
   def destroy
     Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
     # 非同期
-    # render :index
+    render :index
   end
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:comment, :post_id)
+    params.require(:comment).permit(:comment, :post_id, :user_id)
   end
 
 end
