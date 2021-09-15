7d99eb17b746ce164b32c640320151369fd0317a
springkyu <seika.k.r0610@gmail.com>
2021-09-15 11:46:54 +0000

    コメント非同期エラー解消

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -1,10 +1,10 @@
 class Users::CommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    if comment.save!
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save!
       # redirect_back(fallback_location: root_path)
       # 非同期
       render :index
@@ -12,6 +12,7 @@ class Users::CommentsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
     # redirect_back(fallback_location: root_path)
     # 非同期
