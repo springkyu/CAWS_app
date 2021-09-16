635e9897ce5a2e78db4632bc7de74d86a6d8d171
springkyu <seika.k.r0610@gmail.com>
2021-09-16 13:17:25 +0000

    新着順と星評価高い順で投稿一覧を並び替え

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -20,7 +20,8 @@ class Users::PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.page(params[:page]).reverse_order
+    @posts = Post.all.order(params[:sort])
+    @posts_page = Post.page(params[:page]).per(8)
     @user = current_user
   end
 
