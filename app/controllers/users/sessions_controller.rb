cc710b9517390c57515e084e2ff5a1d23769e24f
springkyu <seika.k.r0610@gmail.com>
2021-09-20 09:41:41 +0000

    [UPDATE]userログイン時のバリデーションメッセージ追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  before_action :reject_user, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -32,4 +33,19 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+
+  # ログイン時のバリデーションメッセージ
+  def reject_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+        flash[:alert] = "このアカウントは退会済みです。"
+        redirect_to new_user_session_path
+      end
+    else
+      flash[:alert] = "必須項目を入力してください。"
+    end
+  end
+
 end
