70ef5f3a6ee8336491ff7770cf0f780215338f2a
springkyu <seika.k.r0610@gmail.com>
2021-09-20 14:57:32 +0000

    [UPDATE]searchコントローラ編集

--- app/controllers/admins/searches_controller.rb
+++ app/controllers/admins/searches_controller.rb
@@ -1,2 +1,14 @@
 class Admins::SearchesController < ApplicationController
+
+  def search
+    @model = params[:model]
+    @keyword = params[:keyword]
+    if @model == "Post"
+      @posts = Post.search(@keyword).page(params[:page]).per(8)
+    else
+      @posts = Area.search(@keyword)
+      @posts_area = Post.where(area_id: @posts.ids).page(params[:page]).per(8)
+    end
+  end
+
 end
