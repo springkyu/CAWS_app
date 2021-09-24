8caefca64d68e9e684b668259a9fa1e1db1bd6a9
springkyu <seika.k.r0610@gmail.com>
2021-09-24 10:25:35 +0000

    bootstrap編集・ページネーションきかない

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -41,6 +41,7 @@ class Users::UsersController < ApplicationController
     @user = current_user
     likes = Like.where(user_id: @user.id).pluck(:post_id)
     @like_posts = Post.find(likes)
+    # @like_posts = Post.find(likes).page(params[:page]).per(8)
   end
 
 
